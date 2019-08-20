81a61338280c26264688fc4830dffd2d2a7a62d4
Joe <josephc.lambert@gmail.com>
2019-08-20 13:06:37 -0700

    solves .map() challenge

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -467,7 +467,7 @@ console.log(fullNames);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER.
 // Populate an array called `firstNamesAllCaps`. This array will contain just strings.
-let firstNamesAllCaps = [];
+let firstNamesAllCaps = runners.map(runner => runner.first_name.toUpperCase());
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
